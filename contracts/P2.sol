2fe54b0277ec4ed3ca568847e8024b9c70247f30
Adrian Lawson <adrianglawson@gmail.com>
2022-09-01 09:03:19 -0700

    Added support for ERC20

--- contracts/P2.sol
+++ contracts/P2.sol
@@ -4,6 +4,7 @@ import "../node_modules/openzeppelin-contracts/access/Ownable.sol";
 import "../node_modules/openzeppelin-contracts/utils/ReentrancyGuard.sol";
 import "./Percentages.sol";
 import "../node_modules/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "../node_modules/openzeppelin-contracts/token/ERC20/IERC20.sol";
 
 // 0x0A77230d17318075983913bC2145DB16C7366156 -- AVAX
 
@@ -12,23 +13,33 @@ import "../node_modules/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Int
 */
 
 contract P2 is Ownable, ReentrancyGuard, Percentages{
-    AggregatorV3Interface internal priceFeed;
     constructor() {
-        priceFeed = AggregatorV3Interface(0xdCA36F27cbC4E38aE16C4E9f99D39b42337F6dcf);  // 0x986b5E1e1755e3C2440e960477f25201B0a8bbD4 --main net
+        addToken("ETH", 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
     }
 
-    event sent(string toUsername, string fromUsername, uint256 amount, string memo);
+    event sent(string toUsername, string fromUsername, string ticker, uint256 amount, string memo);
     event funded(string username, address wallet, uint256 amount, uint256 oldBalance, uint256 newBalance);
     event withdrawal(string username, address wallet, uint256 amount, uint256 oldBalance, uint256 newBalance, uint256 fee);
 
+    struct Token {
+        string ticker;
+        address tokenAddress;
+    }
+    mapping(string => Token) public tokenMapping;
+
     struct Account {
         string username;
-        uint256 balance;
         bool hasAccount;
+        mapping(string => uint256) tokenBalances;
     }
     mapping(address => Account) public accounts;
 
     mapping(string => address) public usernameToAddress;
+
+    modifier tokenExists(string memory ticker) {
+        require(tokenMapping[ticker].tokenAddress != address(0), "Token does not exist");
+        _;
+    }
 
     modifier isAccountHolder(string memory _username) {
         require(_msgSender() == usernameToAddress[_username], "Sender is not account holder");
@@ -52,35 +63,49 @@ contract P2 is Ownable, ReentrancyGuard, Percentages{
         usernameToAddress[_username] = _msgSender();
     }
 
-    function fundAccount() external payable nonReentrant {
+    function depositETH() external payable nonReentrant {
         require(accounts[_msgSender()].hasAccount, "Sender has not created an account");
 
-        uint256 oldBalance = accounts[_msgSender()].balance;
+        uint256 oldBalance = accounts[_msgSender()].tokenBalances["ETH"];
 
-        accounts[_msgSender()].balance += msg.value;
+        accounts[_msgSender()].tokenBalances["ETH"] += msg.value;
 
-        emit funded(accounts[_msgSender()].username, _msgSender(), msg.value, oldBalance, accounts[_msgSender()].balance);
+        emit funded(accounts[_msgSender()].username, _msgSender(), msg.value, oldBalance, accounts[_msgSender()].tokenBalances["ETH"]);
     }
 
-    function sendMoney(string calldata _toUsername, uint256 usd, string calldata memo) external isInitialized(_toUsername) nonReentrant {
-        uint256 amount = getUSDtoETH(usd);
-        require(accounts[_msgSender()].balance >= amount, "Insufficient balance");
-        accounts[_msgSender()].balance -= amount;
-        accounts[usernameToAddress[_toUsername]].balance += amount;
+    function depositERC20(string memory ticker, uint256 amount) external tokenExists(ticker){
+        IERC20(tokenMapping[ticker].tokenAddress).transferFrom(msg.sender, address(this), amount);
 
-        emit sent(_toUsername, accounts[_msgSender()].username, amount, memo);
+        accounts[_msgSender()].tokenBalances[ticker] += amount;
     }
 
-    function withdraw(uint256 usd, bool withdrawAll) external nonReentrant {
-        uint256 amount = getUSDtoETH(usd);
+    function sendERC20(string calldata _toUsername, string calldata ticker, uint256 amount, string calldata memo) external tokenExists(ticker) {
+        require(accounts[_msgSender()].tokenBalances[ticker] >= amount, "Insufficient balance");
+        accounts[_msgSender()].tokenBalances[ticker] -= amount;
+
+        accounts[usernameToAddress[_toUsername]].tokenBalances[ticker] += amount;
+
+        emit sent(_toUsername, accounts[_msgSender()].username, ticker, amount, memo);
+    }
+
+    function sendETH(string calldata _toUsername, uint256 amount, string calldata memo) external isInitialized(_toUsername) nonReentrant {
+        require(accounts[_msgSender()].tokenBalances["ETH"] >= amount, "Insufficient balance");
+        accounts[_msgSender()].tokenBalances["ETH"] -= amount;
+        accounts[usernameToAddress[_toUsername]].tokenBalances["ETH"] += amount;
+
+        emit sent(_toUsername, accounts[_msgSender()].username, "ETH", amount, memo);
+    }
+
+    function withdrawETH(uint256 _amount, bool withdrawAll) external nonReentrant {
+        uint256 amount = _amount;
         if(withdrawAll) {
-            amount = accounts[_msgSender()].balance;
+            amount = accounts[_msgSender()].tokenBalances["ETH"];
         }
 
-        require(accounts[_msgSender()].balance >= amount, "Insufficient balance");
+        require(accounts[_msgSender()].tokenBalances["ETH"] >= amount, "Insufficient balance");
 
-        uint256 oldBalance = accounts[_msgSender()].balance;
-        accounts[_msgSender()].balance -= amount;
+        uint256 oldBalance = accounts[_msgSender()].tokenBalances["ETH"];
+        accounts[_msgSender()].tokenBalances["ETH"] -= amount;
 
         uint256 fee = percentageOf(amount, 1) / 4; // 0.25% 
     
@@ -90,34 +115,33 @@ contract P2 is Ownable, ReentrancyGuard, Percentages{
         (bool success2,) = payable(owner()).call{value: fee}("");
         require(success2, 'Transfer fail');
 
-        emit withdrawal(accounts[_msgSender()].username, _msgSender(), amount, oldBalance, accounts[_msgSender()].balance, fee);
+        emit withdrawal(accounts[_msgSender()].username, _msgSender(), amount, oldBalance, accounts[_msgSender()].tokenBalances["ETH"], fee);
     }
 
-    function setPriceFeed(address _pf) external onlyOwner {
-        priceFeed = AggregatorV3Interface(_pf);
+    function withdrawERC20(uint256 amount, string memory ticker) external nonReentrant tokenExists(ticker) {
+        require(accounts[_msgSender()].tokenBalances[ticker] >= amount, "Insufficient balance");
+
+        uint256 oldBalance = accounts[_msgSender()].tokenBalances[ticker];
+        accounts[_msgSender()].tokenBalances[ticker] -= amount;
+
+        uint256 fee = percentageOf(amount, 1) / 4; // 0.25% 
+    
+        IERC20(tokenMapping[ticker].tokenAddress).transferFrom(address(this), _msgSender(), amount - fee);
+        IERC20(tokenMapping[ticker].tokenAddress).transferFrom(address(this), owner(), fee);
+        //accounts[owner()].tokenBalances[ticker] += fee;
+
+        emit withdrawal(accounts[_msgSender()].username, _msgSender(), amount, oldBalance, accounts[_msgSender()].tokenBalances[ticker], fee);
     }
 
-    function usdBalance(string memory _username) public view returns(uint256) {
-        (
-            , // uint80 roundId
-            int256 answer,
-            , // uint256 startedAt
-            , // uint256 updatedAt
-            // uint80 answeredInRound
-        ) = priceFeed.latestRoundData();
-        // answer = 1 / answer; if avax
-        return accounts[usernameToAddress[_username]].balance / uint256(answer);
+    function tokenBalance(string memory ticker, string memory _username) public view returns(uint256) {
+        return accounts[usernameToAddress[_username]].tokenBalances[ticker];
     }
 
-    function getUSDtoETH(uint256 usd) public view returns(uint256) {
-        (
-            , // uint80 roundId
-            int256 answer,
-            , // uint256 startedAt
-            , // uint256 updatedAt
-            // uint80 answeredInRound
-        ) = priceFeed.latestRoundData();
-        // answer = answer / 1; if avax
-        return uint256(answer) * usd;
+    function addToken(string memory ticker, address tokenAddress) public onlyOwner {
+        tokenMapping[ticker] = Token(ticker, tokenAddress);
+    }
+
+    function ownerWithdraw(string memory ticker) external onlyOwner {
+
     }
 }
