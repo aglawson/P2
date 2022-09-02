2ddfe3743cf7848bfaf680275c58e212c2a0cf88
Adrian Lawson <adrianglawson@gmail.com>
2022-09-02 11:47:49 -0700

    Add to test script

--- test/P2.js
+++ test/P2.js
@@ -20,15 +20,53 @@ describe("P2", function () {
   });
 
   describe("Deployment", function () {
-    it('should initialize ETH token at deployment', async function () {
+    it('initializes ETH token at deployment', async function () {
       this.ethObject = await this.p2.tokenMapping("ETH");
       expect(await this.ethObject.tokenAddress).to.equal('0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE');
     });
   });
 
-  describe("Account Creation", function () {
+  describe("Account Functionality", function () {
     it('can create new accounts', async function () {
       expect(await this.p2.connect(this.addr2)['createAccount(string)']('first')).to.emit('accountCreated');
+    });
+
+    it('users can add friends', async function () {
+      // await this.p2.connect(this.addr2)['createAccount(string)']('first');
+      await this.p2.connect(this.addr3)['createAccount(string)']('second');
+      expect(await this.p2.connect(this.addr2)['addFriend(string)']('second')).to.emit('addedFriend');
+    });
+
+    it('can deposit funds', async function () {
+      expect(await this.p2.connect(this.addr2)['depositETH()']({value: '1000000000000000000'})).to.emit('funded');
+      expect(await this.p2.tokenBalance("ETH", "first")).to.equal('1000000000000000000');
+    });
+
+    it('can request funds from friends', async function () {
+      expect(await this.p2.connect(this.addr3)['requestFunds(string,string,uint256,string)']('first', 'ETH', '1000000000000000000', 'freelance work')).to.emit('requestSent');
+    });
+
+    it('can fulfill requests', async function () {
+      //await this.p2.connect(this.addr2)['depositETH()']({value: '1000000000000000000'});
+      expect(await this.p2.connect(this.addr2)['fulfillRequest(uint256)'](0)).to.emit('requestFulfilled');
+      expect(await this.p2.tokenBalance("ETH", "first")).to.equal('0');
+      expect(await this.p2.tokenBalance("ETH", "second")).to.equal('1000000000000000000');
+    });
+
+    it('can send funds', async function () {
+      await this.p2.connect(this.addr4)['createAccount(string)']('HeadlessDev');
+      expect(await this.p2.connect(this.addr3)['sendFunds(string,string,uint256,string)']('HeadlessDev', 'ETH', '50000000000000000', 'For gas money')).to.emit('sent');
+      expect(await this.p2.tokenBalance("ETH", "second")).to.equal('950000000000000000');
+      expect(await this.p2.tokenBalance("ETH", "HeadlessDev")).to.equal('50000000000000000');
+    });
+
+    it('user can withdraw funds', async function () {
+      expect(await this.p2.connect(this.addr4)['withdrawFunds(string,uint256)']('ETH', '50000000000000000')).to.emit('withdrawal');
+      expect(await this.p2.tokenBalance("ETH", "HeadlessDev")).to.equal('0');
+    });
+
+    it('user can remove friends', async function () {
+      expect(await this.p2.connect(this.addr2)['removeFriend(string)']('second')).to.emit('friendRemoved');
     });
   });
  
