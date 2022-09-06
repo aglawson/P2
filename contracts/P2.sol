6c7772f56cfe137244b253cfa6d0ff21de37f64d
Adrian Lawson <adrianglawson@gmail.com>
2022-09-06 10:38:05 -0700

    rename contract

--- contracts/P2.sol
+++ contracts/P2.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.9;
 
 import "./Friends.sol";
 
-contract Cetera is Friends {
+contract P2 is Friends {
     constructor() {
         addToken("ETH", 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE);
     }
